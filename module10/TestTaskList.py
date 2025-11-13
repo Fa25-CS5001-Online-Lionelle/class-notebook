822eb30acd360810bf5247dd9039c043de19000b
Albert Lionelle <a.lionelle@northeastern.edu>
2025-11-13 13:59:29 -0700

    added unit test template

---
+++ module10/TestTaskList.py
@@ -0,0 +1,13 @@
+# Test the various task list features
+# often this will be broken up tinto separate test files - one per object
+import unittest
+
+
+class TestTaskList(unittest.TestCase):
+
+    pass 
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
