ce2b2a026f553a4dccfb969b9880356b872817b4
Albert Lionelle <a.lionelle@northeastern.edu>
2025-11-13 14:01:56 -0700

    updated directory structure

--- module10/tests/TestTaskList.py
+++ module10/tests/TestTaskList.py
@@ -1,7 +1,11 @@
 # Test the various task list features
 # often this will be broken up tinto separate test files - one per object
 import unittest
+import sys
+import os
 
+# Add the parent directory of 'src' to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 class TestTaskList(unittest.TestCase):
 
@@ -10,4 +14,3 @@ class TestTaskList(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
